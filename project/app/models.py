c1ab58797f34c039001c31a5ac6f348b9ff8b442
prabesh0000 <123892028+prabesh0000@users.noreply.github.com>
2024-04-16 22:24:11 +0545

    models

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 from django.db import models
 
 # Create your models here.
@@ -61,11 +62,82 @@ class Testimonial(models.Model):
             verbose_name ='Contact Profile'
             ordering = ["timestamp"]
 
-    timestamp = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(verbose_name="Name",max_length=100)
-    email = models.EmailField(verbose_name="Email")
-    message = models.TextField(verbose_name="Message")
+    thumbnail = models.ImageField(blank=True , null= True , upload_to="testimonial")
+    name = models.CharField(max_length=200, blank=True, null=True)
+    role = models.CharField(max_length=200, blank=True, null=True)
+    quote = models.CharField(max_length=200, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
   
 
     def __str__(self):
-         return f'{self.name}'         
+         return {self.name}        
+    
+
+
+
+
+class Media(models.Model):
+    class meta:
+            verbose_name_plural='Media Files'
+            verbose_name ='Media'
+            ordering = ["name"]
+
+    image = models.ImageField(blank=True , null= True , upload_to="media")
+    url = models.URLField (blank=True, null=True)
+    role = models.CharField(max_length=200, blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    is_image= models.BooleanField(default=True)
+  
+    def save(self, *args , **kwargs):
+         if self.url:
+              self.is_image =  False
+              super(Media, self). save(*args,**kwargs)
+
+
+class Portfolio(models.Model):
+    class meta:
+            verbose_name_plural ='Portfolio Profiles'
+            verbose_name ='Portfolio'
+            ordering = ["name"]
+
+    date = models.DateTimeField(blank=True , null= True)
+    description = models.CharField (max_length=500,blank=True, null=True)
+    body = RichTextField( blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    image = models.ImageField(blank=True, null=True, upload_to="portfolio")
+    slug = models.SlugField(null=True, blank=True)
+    is_active= models.BooleanField(default=True)
+  
+    def save(self, *args , **kwargs):
+         if not self.id:
+              self.slug =  slugify(self.name)
+              super(Portfolio, self). save(*args,**kwargs)   
+
+    def __str__(self):
+         return self.name
+                          
+    def get_deferred_fields(self):
+         return f"/portfolio/{self.slug}"                    
+    
+
+
+class Blog(models.Model):
+    class meta:
+            verbose_name_plural ='Blog Profiles'
+            verbose_name ='Blog'
+            ordering = ["timestamp"]
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=200,blank=True , null= True)
+    description = models.CharField (max_length=500,blank=True, null=True)
+    body = RichTextField( blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    slug = models.SlugField(null=True, blank=True)
+    is_active= models.BooleanField(default=True)
+  
+   
+
+    def __str__(self):
+         return self.name
+                          
+                  
