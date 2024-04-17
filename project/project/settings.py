133d1d98ed04d69649a934986df48ca874324ca6
prabesh0000 <123892028+prabesh0000@users.noreply.github.com>
2024-04-17 22:46:32 +0545

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'project.context_processors.project_contect'
+                'project.context_processors.project_context'
             ],
         },
     },
