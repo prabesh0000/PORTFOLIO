c3971d2377c72f8ac3338a920528af6e705fe512
prabesh0000 <123892028+prabesh0000@users.noreply.github.com>
2024-04-19 14:11:24 +0545

    code failed

--- project/app/urls.py
+++ project/app/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = "app"
 
 urlpatterns = [
-    path('',views.IndexView.as_view(),name="home"),
-    path('contact/',views.ContactView.as_view(),name="contact"),
-    path('portfolio',views.PortfolioView.as_view(),name="portfolios"),
-    path('portfolio/<slug:slug>',views.PortfolioDetailView.as_view(),name="portfolio"),
-    path('blog/',views.BlogView.as_view(),name="blogs"),
-    path('blog/<slug:slug>',views.BlogDetailView.as_view(),name="blog"),
+    path('', views.IndexView.as_view(), name="home"),
+    path('contact/', views.ContactView.as_view(), name="contact"),
+    path('portfolio', views.PortfolioView.as_view(), name="portfolios"),
+    path('portfolio/<slug:slug>', views.PortfolioDetailView.as_view(), name="portfolio"),
+    path('blog/', views.BlogView.as_view(), name="blogs"),
+    path('blog/<slug:slug>', views.BlogDetailView.as_view(), name="blog"),
     
 ]
